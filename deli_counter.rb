785e6dbd613b8001602d3e4aff764c800b182804
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-14 02:18:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else 
-    array.each_with_index {|n, i| puts "The line is currently: #{i+1}. #{n}"}
+    array.each_with_index.chomp {|n, i| puts "The line is currently: #{i+1}. #{n}"}
   end
 end
 
