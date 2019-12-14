ab39419f6e1195eeddace926472c8205e39d7051
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-14 02:24:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
-def line(array)
-  if array.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else 
-    array.each_with_index {|n, i| puts "The line is currently: #{i+1}. #{n}"}
+    katz_deli.each_with_index {|n, i| puts "The line is currently: #{i+1}. #{n}"}
   end
 end
 
-def take_a_number(array, string)
+def take_a_number(katz_deli, string)
   
 end
 
